45d3cad6ed5d15e3876ed68161d951ff7f77414d
rebeacham14 <70616503+rebeacham14@users.noreply.github.com>
2025-12-21 20:46:03 -0800

    Add files via upload

---
+++ angular/src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Storyboard');
+
+  section =signal("Assistant");
+
+  newPprompt = ""
+
+
+  parentVariable = '';
+
+
+
+
+
+}
---
+++ angular/src/app/assistant.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from "@angular/core";
+
+// create ts class
+@Component({
+    selector: 'assistant',
+    template: '<div>assistant</div>'
+})
+export class AssistantComponent {
+}
+
